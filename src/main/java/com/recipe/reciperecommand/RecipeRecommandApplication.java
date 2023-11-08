2c56e0deab41d57f4e8462a7fabe98d31b520698
wooyong99 <wulovesk@naver.com>
2023-11-08 10:02:44 +0900

    chore : aop 기능 활성화

--- src/main/java/com/recipe/reciperecommand/RecipeRecommandApplication.java
+++ src/main/java/com/recipe/reciperecommand/RecipeRecommandApplication.java
@@ -2,12 +2,12 @@ package com.recipe.reciperecommand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class RecipeRecommandApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RecipeRecommandApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeRecommandApplication.class, args);
+    }
 }
